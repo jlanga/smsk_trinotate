10291775ccb34c8cfa6c0aff29be591ee82fbef8
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-02-13 17:13:37 +0100

    Updated smsk

--- src/install/conda_env.sh
+++ src/install/conda_env.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
-
 conda config --add channels conda-forge
 conda config --add channels defaults
 conda config --add channels r
 conda config --add channels bioconda
-conda env create --name smsk_trinotate --file requirements.txt
+conda env create --name smsk_trinotate --file environment.yml
+conda clean --all --yes
