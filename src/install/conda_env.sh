6ebf43d687c8fd5d61cd7aa184d97ac8da5e1de9
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-02-23 17:00:47 +0100

    Use conda channels in the order that bioconda says

--- src/install/conda_env.sh
+++ src/install/conda_env.sh
@@ -3,9 +3,8 @@
 # Create conda environment
 export PATH="$HOME/miniconda3_$TRAVIS_OS_NAME/bin:$PATH"
 #conda env update --file environment.yml
-conda config --add channels conda-forge
 conda config --add channels defaults
-conda config --add channels r
+conda config --add channels conda-forge
 conda config --add channels bioconda
 conda install --yes -c bioconda "snakemake=4.7.0"
 conda clean --all --yes
