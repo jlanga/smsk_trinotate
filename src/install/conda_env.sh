b138670cccb8ebc71ba7ba1210ed62147a1dea6f
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-02-23 11:56:45 +0100

    travis: use conda instead of full environment

--- src/install/conda_env.sh
+++ src/install/conda_env.sh
@@ -2,5 +2,6 @@
 
 # Create conda environment
 export PATH="$HOME/miniconda3_$TRAVIS_OS_NAME/bin:$PATH"
-conda env update --file environment.yml
+#conda env update --file environment.yml
+conda install -c bioconda snakemake
 conda clean --all --yes
