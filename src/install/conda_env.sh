6fab279a60ef7b3c43746b0de3487be5a0fdea0d
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-02-23 16:09:57 +0100

    Specifying snakemake version

--- src/install/conda_env.sh
+++ src/install/conda_env.sh
@@ -3,5 +3,5 @@
 # Create conda environment
 export PATH="$HOME/miniconda3_$TRAVIS_OS_NAME/bin:$PATH"
 #conda env update --file environment.yml
-conda install --yes -c bioconda snakemake
+conda install --yes -c bioconda "snakemake=4.7.0"
 conda clean --all --yes
