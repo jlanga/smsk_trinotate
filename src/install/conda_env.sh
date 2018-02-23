e12a7c261961c1093c352591139e2066f0a5cd74
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-02-23 12:16:10 +0100

    --yes

--- src/install/conda_env.sh
+++ src/install/conda_env.sh
@@ -3,5 +3,5 @@
 # Create conda environment
 export PATH="$HOME/miniconda3_$TRAVIS_OS_NAME/bin:$PATH"
 #conda env update --file environment.yml
-conda install -c bioconda snakemake
+conda install --yes -c bioconda snakemake
 conda clean --all --yes
